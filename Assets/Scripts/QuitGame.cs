2419407c25c0583cfb0376ddded07b20bb94ed31
Olivier Locard <develocard@gmail.com>
2023-04-17 16:07:20 +0200

    Add Exit / Quit game

---
+++ Assets/Scripts/QuitGame.cs
@@ -0,0 +1,10 @@
+using UnityEngine;
+
+public class QuitGame : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public void ButtonPressed()
+    {
+        Application.Quit();
+    }
+}
