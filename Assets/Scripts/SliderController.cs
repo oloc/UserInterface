348ffb02bc1875a33d3d3a46591a5a947bd3f0f0
Olivier Locard <develocard@gmail.com>
2023-04-18 11:48:21 +0200

    KISS: Slider simplification

--- Assets/Scripts/SliderController.cs
+++ Assets/Scripts/SliderController.cs
@@ -1,22 +1,12 @@
-using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
 
 public class SliderController : MonoBehaviour
 {
-    [SerializeField]
-    private TextMeshProUGUI _textMesh;
-
     private Slider _slider;
 
     private void Awake()
     {
         _slider = GetComponent<Slider>();
-        _textMesh.text = _slider.value.ToString("F2");
-    }
-
-    public void onSliderValueChanged(float value)
-    {
-        _textMesh.text = value.ToString("F2");
     }
 }
