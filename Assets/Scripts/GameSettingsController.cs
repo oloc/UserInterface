f5ead25e237463d7532f36112a3727642f698bf8
Olivier Locard <develocard@gmail.com>
2023-04-18 14:06:28 +0200

    Add Music Volume Settings

--- Assets/Scripts/GameSettingsController.cs
+++ Assets/Scripts/GameSettingsController.cs
@@ -5,11 +5,18 @@ public class GameSettingsController : MonoBehaviour
 {
     [SerializeField]
     private AudioMixer _audioMixer;
+
     private const string MAINVOLUME_PARAM = "MainVolume";
+    private const string MUSICVOLUME_PARAM = "MusicVolume";
 
     public void SetMainVolume(float volume)
     {
         _audioMixer.SetFloat(MAINVOLUME_PARAM, Mathf.Log10(volume) * 20);
+    }
+
+    public void SetMusicVolume(float volume)
+    {
+        _audioMixer.SetFloat(MUSICVOLUME_PARAM, Mathf.Log10(volume) * 20);
     }
 
 }
