c19e6afd4418207608750cbd70cba1299bf827ab
Olivier Locard <develocard@gmail.com>
2023-04-18 16:10:44 +0200

    Add player prefs audio save

--- Assets/Scripts/GameSettingsController.cs
+++ Assets/Scripts/GameSettingsController.cs
@@ -1,14 +1,44 @@
 using UnityEngine;
 using UnityEngine.Audio;
+using UnityEngine.UI;
 
 public class GameSettingsController : MonoBehaviour
 {
     [SerializeField]
     private AudioMixer _audioMixer;
+    [SerializeField]
+    private Slider _mainVolumeSlider;
+    [SerializeField]
+    private Slider _musicVolumeSlider;
+    [SerializeField]
+    private Slider _sfxVolumeSlider;
 
-    private const string MAINVOLUME_PARAM = "MainVolume";
-    private const string MUSICVOLUME_PARAM = "MusicVolume";
-    private const string SFXVOLUME_PARAM = "SFXVolume";
+    // Exposed mixer parameters
+    public const string MAINVOLUME_PARAM = "MainVolume";
+    public const string MUSICVOLUME_PARAM = "MusicVolume";
+    public const string SFXVOLUME_PARAM = "SFXVolume";
+
+    // PlayerPrefs keys
+    public const string PLAYERPREFS_MAINVOLUME = "Sound:MainVolume";
+    public const string PLAYERPREFS_MUSICVOLUME = "Sound:MusicVolume";
+    public const string PLAYERPREFS_SFXVOLUME = "Sound:SFXVolume";
+
+
+    private void Start()
+    {
+        SlideVolume(_mainVolumeSlider, PLAYERPREFS_MAINVOLUME);
+        SlideVolume(_musicVolumeSlider, PLAYERPREFS_MUSICVOLUME);
+        SlideVolume(_sfxVolumeSlider, PLAYERPREFS_SFXVOLUME);
+    }
+
+    private void SlideVolume(Slider slider, string key)
+    {
+        if (PlayerPrefs.HasKey(key))
+        {
+            float playerPrefsVolume = PlayerPrefs.GetFloat(key);
+            slider.value = playerPrefsVolume;
+        }
+    }
 
     public void SetDialogSpeed(float dialogSpeed)
     {
@@ -18,16 +48,22 @@ public class GameSettingsController : MonoBehaviour
     public void SetMainVolume(float volume)
     {
         _audioMixer.SetFloat(MAINVOLUME_PARAM, Mathf.Log10(volume) * 20);
+        PlayerPrefs.SetFloat(PLAYERPREFS_MAINVOLUME, volume);
+        PlayerPrefs.Save();
     }
 
     public void SetMusicVolume(float volume)
     {
         _audioMixer.SetFloat(MUSICVOLUME_PARAM, Mathf.Log10(volume) * 20);
+        PlayerPrefs.SetFloat(PLAYERPREFS_MUSICVOLUME, volume);
+        PlayerPrefs.Save();
     }
 
     public void SetSFXVolume(float volume)
     {
         _audioMixer.SetFloat(SFXVOLUME_PARAM, Mathf.Log10(volume) * 20);
+        PlayerPrefs.SetFloat(PLAYERPREFS_SFXVOLUME, volume);
+        PlayerPrefs.Save();
     }
 
     public void SetToolTips(bool toggle)
