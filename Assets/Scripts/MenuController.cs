8aba4b38b2f4825d5fdb0c5803a0e9003ea2f599
Olivier Locard <develocard@gmail.com>
2023-04-18 09:22:54 +0200

    Refacto: Menu buttons scripts in one MenuController

--- Assets/Scripts/MenuController.cs
+++ Assets/Scripts/MenuController.cs
@@ -1,18 +1,31 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class MenuController : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField]
+    private string _settingsScene;
+    [SerializeField]
+    private CanvasGroup _canvasGroupToActivate;
+
+    private void Awake()
     {
-        
+        _canvasGroupToActivate.interactable = false;
     }
 
-    // Update is called once per frame
-    void Update()
+    public void PlayButtonPressed()
     {
-        
+        _canvasGroupToActivate.interactable = !_canvasGroupToActivate.interactable;
+        _canvasGroupToActivate.alpha = _canvasGroupToActivate.interactable ? 1f : 0f;
+    }
+
+    public void SettingsButtonPressed()
+    {
+        SceneManager.LoadScene(_settingsScene);
+    }
+
+    public void QuitButtonPressed()
+    {
+        Application.Quit();
     }
 }
