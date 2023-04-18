43198590f47305bc1284776b6f109d33c1e7ee7b
Olivier Locard <develocard@gmail.com>
2023-04-18 18:14:51 +0200

    Add player prefs Fullscreen and Vsync save

--- Assets/Scripts/VideoSettingsController.cs
+++ Assets/Scripts/VideoSettingsController.cs
@@ -1,12 +1,19 @@
 using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
-public class VideoSettingsController : MonoBehaviour
+public class VideoSettingsController : SettingsController
 {
     [SerializeField]
     private TMP_Dropdown _qualityDropdown;
+    [SerializeField]
+    private Toggle _fullScreen;
+    [SerializeField]
+    private Toggle _vSync;
 
     public const string PLAYERPREFS_QUALITYLEVEL = "Graphics:QualityLevel";
+    public const string PLAYERPREFS_FULLSCREEN = "Graphics:FullScreen";
+    public const string PLAYERPREFS_VSYNC = "Graphics:Vsync";
 
     // Start is called before the first frame update
     void Start()
@@ -15,11 +22,24 @@ public class VideoSettingsController : MonoBehaviour
         {
             _qualityDropdown.value = PlayerPrefs.GetInt(PLAYERPREFS_QUALITYLEVEL);
         }
+
+        SetToggleFromPrefs(_fullScreen, PLAYERPREFS_FULLSCREEN);
+        SetToggleFromPrefs(_vSync, PLAYERPREFS_VSYNC);
     }
 
     public void UpdateQualityLevel(int qualityLevel)
     {
         QualitySettings.SetQualityLevel(qualityLevel);
         PlayerPrefs.SetInt(PLAYERPREFS_QUALITYLEVEL, qualityLevel);
+    }
+
+    public void UpdateFullScreen(bool toggle)
+    {
+        SaveToggleInPrefs(PLAYERPREFS_FULLSCREEN, toggle);
+    }
+
+    public void UpdateVsync(bool toggle)
+    {
+        SaveToggleInPrefs(PLAYERPREFS_VSYNC, toggle);
     }
 }
